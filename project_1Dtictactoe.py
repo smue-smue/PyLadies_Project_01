891c23ce039467d856f621015c8b514ff593a73c
Sandra Müllner <s_mue_@outlook.com>
2023-11-25 12:01:58 +0100

    Fixed ingame evaluation. Now it shouldn't be possible that both players are winning.

--- project_1Dtictactoe.py
+++ project_1Dtictactoe.py
@@ -66,6 +66,7 @@ def fun_pc_move(current_mark, pos_number, board, rand_number):
             break
         else:
             rand_number = randrange(0,19)
+    
     return fun_move(board, pos_number, current_mark)
 
 # STEP 5
@@ -73,30 +74,21 @@ def fun_pc_move(current_mark, pos_number, board, rand_number):
 def fun_1D_tictactoe(current_mark, pos_number, board, rand_number, result):
     '''Creates string, alternatively calls player_move and pc_move functions until the end of the game.'''
     
+    result = fun_evaluate(board)
     while True:
-        result = fun_evaluate(board)
         if result == "-":
             board = fun_player_move(current_mark, pos_number, board, input_number)
-            board = fun_pc_move(current_mark, pos_number, board, rand_number)
-            fun_1D_tictactoe(current_mark, pos_number, board, rand_number, result)
+            result = fun_evaluate(board)
+            if result == "-":
+                board = fun_pc_move(current_mark, pos_number, board, rand_number)
+                result = fun_evaluate(board)
+            else: 
+                print("The game ends. You won. \nThis is the final board:", board, "\n")
+                break
         else:
-            print("The game ends. \nThis is the final board:", board, "\n")
-        break
+            print("The game ends. Computer won. \nThis is the final board:", board, "\n")
+            break
 
 # PLAY
-
-#print(board) # prints board
-#print(len(board)) # prints board length for checking
-#print("Status: ", fun_evaluate(board)) # prints game status
-
-#board = fun_player_move(current_mark, pos_number, board)
-#print(board)
-#print(len(board)) # prints board length for checking
-#print("Status: ", fun_evaluate(board)) # prints game status
-
-#board = fun_pc_move(current_mark, pos_number, board, rand_number)
-#print(board)
-#print(len(board)) # prints board length for checking
-#print("Status: ", fun_evaluate(board)) # prints game status
 
 board = fun_1D_tictactoe(current_mark, pos_number, board, rand_number, result)
