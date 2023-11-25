1a3913db95c35e7d87ddc105d5daa28292ca5779
Sandra Müllner <s_mue_@outlook.com>
2023-11-25 14:09:14 +0100

    Did some general cleaning.

--- project_1Dtictactoe.py
+++ project_1Dtictactoe.py
@@ -26,7 +26,7 @@ def fun_evaluate(board):
 # STEP 2
 
 def fun_move(board, pos_number, current_mark):
-    '''Accepts string of the game board, a position number (0-19), and a mark (x or o). \n
+    '''Accepts string of the game board, a position number (0-19), and a mark (x or o).
     Then returns the updated game board.'''
 
     list_board = list(board)
@@ -36,12 +36,12 @@ def fun_move(board, pos_number, current_mark):
 
 # STEP 3
 
-def fun_player_move(current_mark, pos_number, board, input_number):
-    '''Accepts string of the game board, asks player which position \n 
+def fun_player_move(board, current_mark, pos_number, input_number):
+    '''Accepts string of the game board, asks player which position 
     to play, returns updated board.'''
 
     current_mark = player_mark
-    print(board)
+    print("\n", board)
     input_number = int(input("Which position you would like to play? "))
     
     if board[input_number] == "-":
@@ -49,17 +49,18 @@ def fun_player_move(current_mark, pos_number, board, input_number):
     else:
         input_number = None
         print("This position is not free, please choose another one.")
-        return fun_player_move(current_mark, pos_number, board, input_number)
+        return fun_player_move(board, current_mark, pos_number, input_number)
     
     return fun_move(board, pos_number, current_mark)
 
 # STEP 4
 
-def fun_pc_move(current_mark, pos_number, board, rand_number):
-    '''Accepts string of the game board, will select position to play, returns the updated board.'''
+def fun_pc_move(board, current_mark, pos_number, rand_number):
+    '''Accepts string of the game board, will select position to play, 
+    returns the updated board.'''
 
     current_mark = computer_mark
-    rand_number = randrange(0,19)
+    rand_number = randrange(0, 19)
     while True:
         if board[rand_number] == "-":
             pos_number = rand_number
@@ -71,24 +72,24 @@ def fun_pc_move(current_mark, pos_number, board, rand_number):
 
 # STEP 5
 
-def fun_1D_tictactoe(current_mark, pos_number, board, rand_number, result):
+def fun_1D_tictactoe(board, current_mark, pos_number, rand_number, result):
     '''Creates string, alternatively calls player_move and pc_move functions until the end of the game.'''
     
     result = fun_evaluate(board)
     while True:
         if result == "-":
-            board = fun_player_move(current_mark, pos_number, board, input_number)
+            board = fun_player_move(board, current_mark, pos_number, input_number)
             result = fun_evaluate(board)
             if result == "-":
-                board = fun_pc_move(current_mark, pos_number, board, rand_number)
+                board = fun_pc_move(board, current_mark, pos_number, rand_number)
                 result = fun_evaluate(board)
             else: 
-                print("The game ends. You won. \nThis is the final board:", board, "\n")
+                print("\nCongratulations, you won. \nThis is the final board:", board, "\n")
                 break
         else:
-            print("The game ends. Computer won. \nThis is the final board:", board, "\n")
+            print("\nComputer won. \nThis is the final board:", board, "\n")
             break
 
 # PLAY
 
-board = fun_1D_tictactoe(current_mark, pos_number, board, rand_number, result)
+board = fun_1D_tictactoe(board, current_mark, pos_number, rand_number, result)
